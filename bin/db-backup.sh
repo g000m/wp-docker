0edc53c84bcfacb7df2ce7dd01e2ad6cdf44048b
Derek Moon <dmoon@forumone.com>
2017-08-02 00:16:03 -0700

    update backup script

--- bin/db-backup.sh
+++ bin/db-backup.sh
@@ -5,4 +5,4 @@ timestamp=$(date "+%d%M%Y-%H%S")
 docker-compose run wpcli db export backup.sql &&
 mv ./public/backup.sql ./data/backup.sql &&
 # cp ./data/backup.sql ./data/backup-$timestamp.sql &# &
-echo "DB backup-$timestamp.sql has been backed up to the data folder"
+echo "Database backup.sql has been created in the data folder"
