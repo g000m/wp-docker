f1768084ad78983311e93936dad8dead55d0e918
Derek Moon <dmoon79@gmail.com>
2017-10-07 01:51:28 -0700

    Cleanup db backup script

--- bin/db-backup.sh
+++ bin/db-backup.sh
@@ -4,5 +4,4 @@
 
 docker-compose run wpcli db export backup.sql &&
 mv ./public/backup.sql ./data/backup.sql &&
-# cp ./data/backup.sql ./data/backup-$timestamp.sql &# &
 echo "Database backup.sql has been created in the data folder"
